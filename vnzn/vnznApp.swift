07a754b568521fc950196db33e1432fad7be45fe
Christian Noll <christiannoll@gmx.net>
2024-11-09 19:25:24 +0100

    First commit

---
+++ vnzn/vnznApp.swift
@@ -0,0 +1,17 @@
+//
+//  vnznApp.swift
+//  vnzn
+//
+//  Created by Christian on 09.11.24.
+//
+
+import SwiftUI
+
+@main
+struct vnznApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ vnznTests/vnznTests.swift
@@ -0,0 +1,17 @@
+//
+//  vnznTests.swift
+//  vnznTests
+//
+//  Created by Christian on 09.11.24.
+//
+
+import Testing
+@testable import vnzn
+
+struct vnznTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
