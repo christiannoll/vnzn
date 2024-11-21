fb5caf996ed3fefda9b0167318a831a5f4b5dcd2
Christian Noll <christiannoll@gmx.net>
2024-11-21 20:01:33 +0100

    Extracted PostRow view

---
+++ vnzn/Components/PostRow.swift
@@ -0,0 +1,53 @@
+import SwiftUI
+
+struct PostRow: View {
+    
+    let post: Post
+    @Binding var selectedPost: Post?
+    @Binding var path: NavigationPath
+    
+    var body: some View {
+        if post.type == PostType.text {
+            Button {
+                selectedPost = post
+                path.append(post)
+            } label: {
+                VStack(alignment: .leading) {
+                    Text(post.title)
+                        .padding(.top, 20)
+                    Text(createPostDate(post)).foregroundStyle(.secondary)
+                }
+            }
+            .buttonStyle(.plain)
+            .listRowSeparator(.hidden)
+            .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+            .listRowBackground(Color.clear)
+        } else {
+            VStack(alignment: .center) {
+                HStack {
+                    Spacer()
+                    Button {
+                        selectedPost = post
+                        path.append(post)
+                    } label: {
+                        PostImage(post: post)
+                            .frame(width: 200, height: 200)
+                            .padding(.top, 30)
+                    }
+                    Spacer()
+                }
+                Text(createPostDate(post)).foregroundStyle(.secondary)
+                
+            }
+            .listRowBackground(Color.clear)
+        }
+    }
+    
+    private func createPostDate(_ post: Post) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "de_DE")
+        dateFormatter.dateFormat = "dd MMM yyyy"
+        return dateFormatter.string(from: post.date!)
+    }
+}
+
--- vnzn/View/TimelineView.swift
+++ vnzn/View/TimelineView.swift
@@ -23,40 +23,7 @@ struct TimelineView: View {
         NavigationStack(path: $path) {
             List {
                 ForEach (searchResults.filter { shouldInclude($0) }) { post in
-                    if post.type == PostType.text {
-                        Button {
-                            selectedPost = post
-                            path.append(post)
-                        } label: {
-                            VStack(alignment: .leading) {
-                                Text(post.title)
-                                    .padding(.top, 20)
-                                Text(createPostDate(post)).foregroundStyle(.secondary)
-                            }
-                        }
-                        .buttonStyle(.plain)
-                        .listRowSeparator(.hidden)
-                        .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
-                        .listRowBackground(Color.clear)
-                    } else {
-                        VStack(alignment: .center) {
-                            HStack {
-                                Spacer()
-                                Button {
-                                    selectedPost = post
-                                    path.append(post)
-                                } label: {
-                                    PostImage(post: post)
-                                        .frame(width: 200, height: 200)
-                                        .padding(.top, 30)
-                                }
-                                Spacer()
-                            }
-                            Text(createPostDate(post)).foregroundStyle(.secondary)
-                            
-                        }
-                        .listRowBackground(Color.clear)
-                    }
+                    PostRow(post: post, selectedPost: $selectedPost, path: $path)
                 }
             }
             .navigationDestination(for: Post.self) { post in
@@ -69,13 +36,6 @@ struct TimelineView: View {
             .navigationBarTitleTextColor(.blue)
             .scrollContentBackground(.hidden)
         }
-    }
-    
-    private func createPostDate(_ post: Post) -> String {
-        let dateFormatter = DateFormatter()
-        dateFormatter.locale = Locale(identifier: "de_DE")
-        dateFormatter.dateFormat = "dd MMM yyyy"
-        return dateFormatter.string(from: post.date!)
     }
     
     private func shouldInclude(_ post: Post) -> Bool {
