75cfcc5ce7c89d158d2dd14bd154b8e74c7a2563
Christian Noll <christiannoll@gmx.net>
2024-11-24 14:10:58 +0100

    Partly implemented HistoryView

--- vnzn/View/HistoryView.swift
+++ vnzn/View/HistoryView.swift
@@ -3,18 +3,46 @@ import SwiftData
 
 struct HistoryView: View {
     
+    @State private var path = NavigationPath()
+    
+    @Environment(\.modelContext) private var modelContext
     @Query(sort: \HistoryItem.date, order: .reverse) var items: [HistoryItem]
     
     var body: some View {
-        NavigationStack {
+        NavigationStack(path: $path) {
             List {
                 ForEach(items) { item in
-                    HStack {
-                        Text(item.post?.title ?? "")
-                        Text(item.date?.description ?? "")
+                    Button {
+                        path.append(item.post!)
+                    } label: {
+                        HStack {
+                            Text(item.post?.title ?? "")
+                            Text(item.date?.description ?? "")
+                        }
+                    }
+                    .buttonStyle(.plain)
+                }
+            }
+            .navigationDestination(for: Post.self) { post in
+                PostView(post: post, path: $path)
+            }
+            .navigationTitle("Verlauf")
+            .scrollContentBackground(.hidden)
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("Löschen") {
+                        deleteHistory()
                     }
                 }
             }
+        }
+    }
+    
+    private func deleteHistory() {
+        do {
+            try modelContext.delete(model: HistoryItem.self)
+        } catch {
+            print("Failed to delete all schools.")
         }
     }
 }
