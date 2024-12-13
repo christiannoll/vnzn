9b8fa3fccf7cde6fce0b5be32e81a8b7e4434d74
Christian Noll <christiannoll@gmx.net>
2024-12-13 19:11:19 +0100

    Added section with random post in DiscoverView

--- vnzn/View/DiscoverView.swift
+++ vnzn/View/DiscoverView.swift
@@ -3,20 +3,59 @@ import SwiftData
 
 struct DiscoverView: View {
 
-    @Query(sort: \Post.date) var posts: [Post]
+    @State private var selectedPost: Post? = nil
 
+    @Query(sort: \Post.date) var posts: [Post]
     @Environment(Router.self) var router: Router
 
     var body: some View {
         @Bindable var router = router
         NavigationStack(path: $router.discoverViewNavigationPath) {
-            Section {
-                PostView(post: posts[Int.random(in: 0..<posts.count)])
+            List {
+                let post = posts[Int.random(in: 0..<posts.count)]
+                Section("Zufälliger Post") {
+                    if post.type == PostType.text {
+                        Button {
+                            selectedPost = post
+                            router.currentNavigationPath.append(NavigationTarget.post(post))
+                        } label: {
+                            VStack(alignment: .leading) {
+                                Text(post.title)
+                                Text(createPostDate(post)).foregroundStyle(.secondary)
+                            }
+                        }
+                        .buttonStyle(.plain)
+                    } else {
+                        VStack(alignment: .center) {
+                            HStack {
+                                Spacer()
+                                Button {
+                                    selectedPost = post
+                                    router.currentNavigationPath.append(NavigationTarget.post(post))
+                                } label: {
+                                    PostImage(post: post)
+                                        .frame(width: 200, height: 200)
+                                        .padding(.top, 30)
+                                }
+                                Spacer()
+                            }
+                            Text(createPostDate(post)).foregroundStyle(.secondary)
+
+                        }
+                    }
+                }
             }
             .selectNavigationDestination()
             .navigationTitle("Entdecken")
             .navigationBarTitleDisplayMode(.inline)
             .toolbarBackground(.visible, for: .tabBar)
         }
+    }
+
+    private func createPostDate(_ post: Post) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "de_DE")
+        dateFormatter.dateFormat = "dd MMM yyyy"
+        return dateFormatter.string(from: post.date!)
     }
 }
