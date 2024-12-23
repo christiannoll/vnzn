e30d25dc051f117e111c00af825a637581999441
Christian Noll <christiannoll@gmx.net>
2024-12-23 18:07:24 +0100

    Added a lock in function createIndex

--- vnzn/Model/Index.swift
+++ vnzn/Model/Index.swift
@@ -4,7 +4,8 @@ import Foundation
 class Index {
     
     var indexItems: [IndexItem] = []
-    
+    private let lock = NSLock()
+
     var numberOfIndexItems: Int {
         get { return indexItems.count }
     }
@@ -32,18 +33,20 @@ class Index {
     
     private func getIndexItems(_ post: Post) async -> [IndexItem] {
         var _indexItems: [IndexItem] = []
-        for index in post.indices {
-            var found = false
-            for indexItem in indexItems {
-                if index == indexItem.key {
-                    _indexItems.append(indexItem)
-                    found = true
+        lock.withLock {
+            for index in post.indices {
+                var found = false
+                for indexItem in indexItems {
+                    if index == indexItem.key {
+                        _indexItems.append(indexItem)
+                        found = true
+                    }
                 }
-            }
-            if !found {
-                let indexItem = IndexItem(index)
-                _indexItems.append(indexItem)
-                indexItems.append(indexItem)
+                if !found {
+                    let indexItem = IndexItem(index)
+                    _indexItems.append(indexItem)
+                    indexItems.append(indexItem)
+                }
             }
         }
         return _indexItems
