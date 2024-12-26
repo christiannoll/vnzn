d69e70088fba47f5e785189482ceab0ead4c7503
Christian Noll <christiannoll@gmx.net>
2024-12-26 14:47:15 +0100

    Added language support

--- vnzn/VnznEnv.swift
+++ vnzn/VnznEnv.swift
@@ -2,6 +2,17 @@ import Foundation
 
 struct VnznEnv {
     
-    static let baseUrl = "http://localhost:8000/"
-    //static let baseUrl = "https://www.vnzn.de/"
+    static var baseUrl: String {
+        if let lang = Locale.preferredLanguages.first {
+            if lang.contains("en") {
+                return "http://localhost:8000/en/"
+                // return "https://www.vnzn.de/en/"
+            }
+        }
+        return "http://localhost:8000/"
+        // return "https://www.vnzn.de/"
+    }
+
+    public static let baseRootUrl = "http://localhost:8000/"
+    //public static let baseRootUrl = "https://www.vnzn.de/"
 }
