121351af320281281a9d8eeefbca773c403244f0
Christian Noll <christiannoll@gmx.net>
2025-01-16 21:11:45 +0100

    Added widget extension

---
+++ vnznWidget/vnznWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  vnznWidgetBundle.swift
+//  vnznWidget
+//
+//  Created by Christian on 16.01.25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct vnznWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        vnznWidget()
+        vnznWidgetControl()
+    }
+}
