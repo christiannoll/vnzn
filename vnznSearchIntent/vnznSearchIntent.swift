f3401d9ab1765a0131b6dd047542971648342627
Christian Noll <christiannoll@gmx.net>
2025-02-14 10:30:00 +0100

    Added a vnznAppShortcutsProvider

--- vnznSearchIntent/vnznSearchIntent.swift
+++ vnznSearchIntent/vnznSearchIntent.swift
@@ -1,9 +1,25 @@
 import AppIntents
 
 struct vnznSearchIntent: AppIntent {
-    static var title: LocalizedStringResource { "vnznSearchIntent" }
-    
+
+    static var title: LocalizedStringResource = "Search"
+    static let description: LocalizedStringResource = "Find a post"
+
+    @MainActor
     func perform() async throws -> some IntentResult {
         return .result()
+    }
+}
+
+struct vnznAppShortcutsProvider: AppShortcutsProvider {
+    static var appShortcuts: [AppShortcut] {
+        AppShortcut(
+            intent: vnznSearchIntent(),
+            phrases: [
+                "Find a \(.applicationName) post"
+            ],
+            shortTitle: "Search for a post",
+            systemImageName: "magnifyingglass"
+        )
     }
 }
