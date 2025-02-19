0d75c0a1e73a9a44ee0b0b806c22b3d8f9dfa5a5
Christian Noll <christiannoll@gmx.net>
2025-02-19 20:56:52 +0100

    Support device orientation at photos view

--- vnzn/View/PhotosView.swift
+++ vnzn/View/PhotosView.swift
@@ -3,13 +3,34 @@ import SwiftUI
 struct PhotosView: View {
     
     @State private var posts: [Post] = []
+    @State private var orientation = UIDeviceOrientation.unknown
+
     @Environment(MetaData.self) var metaData: MetaData
 
+    let columns = [
+        GridItem(.adaptive(minimum: 220))
+    ]
+
     var body: some View {
-        List {
-            ForEach (posts) { post in
-                PostRow(post: post)
+        Group {
+            if orientation.isLandscape {
+                ScrollView {
+                    LazyVGrid(columns: columns, spacing: 10) {
+                        ForEach (posts) { post in
+                            PostRow(post: post)
+                        }
+                    }
+                }
+            } else {
+                List {
+                    ForEach (posts) { post in
+                        PostRow(post: post)
+                    }
+                }
             }
+        }
+        .onRotate { newOrientation in
+            orientation = newOrientation
         }
         .onAppear {
             if let tagItem = metaData.tags.getTagItem("Foto") {
