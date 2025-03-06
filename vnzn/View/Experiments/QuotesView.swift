3686c0d595fb285712befa6f9fa85f1c78f13be5
Christian Noll <christiannoll@gmx.net>
2025-03-06 20:01:54 +0100

    Use IndexItemView in QuotesView

--- vnzn/View/Experiments/QuotesView.swift
+++ vnzn/View/Experiments/QuotesView.swift
@@ -1,11 +1,23 @@
 import SwiftUI
+import SwiftData
 
 struct QuotesView: View {
-    
-    var body: some View {
-        List {
 
+    @Environment(MetaData.self) var metaData: MetaData
+    @Query(sort: \Post.date, order: .reverse) var posts: [Post]
+
+    var body: some View {
+        Group {
+            if let indexItem = metaData.index.getIndexItem("Zitat") {
+                IndexItemView(indexItem: indexItem)
+            } else {
+                Text("Keinen passenden Post gefunden. 🙁")
+            }
         }
-        .navigationTitle("Zitate")
+        .task {
+            if metaData.index.indexItems.isEmpty {
+                await metaData.index.createIndex(posts)
+            }
+        }
     }
 }
