dfda8a9f87aefc70b5cdd38f9bff882f30988f91
Christian Noll <christiannoll@gmx.net>
2025-06-30 21:07:53 +0200

    Customized SafariWebView

--- vnzn/View/SafariWebView.swift
+++ vnzn/View/SafariWebView.swift
@@ -5,8 +5,24 @@ struct SafariWebView: View {
 
     let url: URL
 
+    @State private var page = WebPage()
+    @State private var findNavigatorIsPresented = false
+
     var body: some View {
-        WebView(url: url)
+        WebView(page)
             .toolbar(.hidden, for: .tabBar)
+            .navigationTitle(page.title)
+            .onAppear {
+                page.load(URLRequest(url: url))
+            }
+            .ignoresSafeArea(.all, edges: .bottom)
+            .findNavigator(isPresented: $findNavigatorIsPresented)
+            .toolbar {
+                ToolbarItemGroup {
+                    Button("Find", systemImage: "magnifyingglass") {
+                        findNavigatorIsPresented.toggle()
+                    }
+                }
+            }
     }
 }
