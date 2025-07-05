01eaee868ecfe6b70aefc5222635abab21ea0fa2
Christian Noll <christiannoll@gmx.net>
2025-07-05 18:59:27 +0200

    Use Tabs in TabView instead of tabItem modifier

--- vnzn/View/ContentView.swift
+++ vnzn/View/ContentView.swift
@@ -7,53 +7,21 @@ struct ContentView: View {
     var body: some View {
         @Bindable var router = router
         TabView(selection: $router.selectedTab) {
-            PostsView()
-                .tabItem {
-                    if router.selectedTab == .posts {
-                        Label("Blog", systemImage: "rectangle.stack.fill")
-                    } else {
-                        Label("Blog", systemImage: "rectangle.stack")
-                    }
-                }
-                .tag(Router.Tabs.posts)
-            DiscoverView()
-                .tabItem {
-                    if router.selectedTab == .discover {
-                        Label("Entdecken", systemImage: "globe")
-                            .foregroundStyle(Color.accentColor)
-                    } else {
-                        Label("Entdecken", systemImage: "globe")
-                    }
-                }
-                .tag(Router.Tabs.discover)
-            IndexView()
-                .tabItem {
-                    if router.selectedTab == .index {
-                        Label("Index", systemImage: "list.bullet")
-                            .foregroundStyle(Color.accentColor)
-                    } else {
-                        Label("Index", systemImage: "list.bullet")
-                    }
-                }
-                .tag(Router.Tabs.index)
-            HistoryView()
-                .tabItem {
-                    if router.selectedTab == .history {
-                        Label("Verlauf", systemImage: "clock.fill")
-                    } else {
-                        Label("Verlauf", systemImage: "clock")
-                    }
-                }
-                .tag(Router.Tabs.history)
-            MetaView()
-                .tabItem {
-                    if router.selectedTab == .meta {
-                        Label("Meta", systemImage: "ellipsis.circle.fill")
-                    } else {
-                        Label("Meta", systemImage: "ellipsis.circle")
-                    }
-                }
-                .tag(Router.Tabs.meta)
+            Tab("Blog", systemImage: "rectangle.stack", value: .posts) {
+                PostsView()
+            }
+            Tab("Entdecken", systemImage: "globe", value: .discover) {
+                DiscoverView()
+            }
+            Tab("Index", systemImage: "list.bullet", value: .index) {
+                IndexView()
+            }
+            Tab("Verlauf", systemImage: "clock", value: .history) {
+                HistoryView()
+            }
+            Tab("Meta", systemImage: "ellipsis.circle", value: .meta) {
+                MetaView()
+            }
         }
     }
 }
