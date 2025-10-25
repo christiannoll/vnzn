fe09fc069287de0300467e5ca9d98d078dc5c024
Christian Noll <christiannoll@gmx.net>
2025-10-25 14:45:25 +0200

    Added search field in search view

--- vnzn/View/SearchView.swift
+++ vnzn/View/SearchView.swift
@@ -3,62 +3,73 @@ import SwiftData
 
 struct SearchView: View {
 
+    @State private var searchText = ""
     @Query(sort: \Post.date) var posts: [Post]
 
     @Environment(MetaData.self) var metaData: MetaData
     @Environment(Router.self) var router: Router
 
+    @FocusState private var isSearchFieldFocused: Bool
+
     var body: some View {
         @Bindable var router = router
         NavigationStack(path: $router.searchViewNavigationPath) {
-            ScrollView {
-                LazyVGrid(
-                    columns: [
-                        GridItem(.flexible(minimum: 50, maximum: .infinity)),
-                        GridItem(.flexible(minimum: 50, maximum: .infinity))
-                    ],
-                    alignment: .leading,
-                    spacing: 10
-                ) {
-                    ForEach (metaData.tags.tagItems) { tagItem in
-                        Button {
-                            router.currentNavigationPath.append(NavigationTarget.tag(tagItem))
-                        } label: {
-                            VStack(alignment: .leading) {
-                                HStack {
-                                    Spacer()
-                                    Image(systemName: "star")
-                                        .padding(.horizontal)
-                                        .foregroundStyle(.background)
+            Group {
+                if isSearchFieldFocused {
+                    
+                } else {
+                    ScrollView {
+                        LazyVGrid(
+                            columns: [
+                                GridItem(.flexible(minimum: 50, maximum: .infinity)),
+                                GridItem(.flexible(minimum: 50, maximum: .infinity))
+                            ],
+                            alignment: .leading,
+                            spacing: 10
+                        ) {
+                            ForEach (metaData.tags.tagItems) { tagItem in
+                                Button {
+                                    router.currentNavigationPath.append(NavigationTarget.tag(tagItem))
+                                } label: {
+                                    VStack(alignment: .leading) {
+                                        HStack {
+                                            Spacer()
+                                            Image(systemName: "star")
+                                                .padding(.horizontal)
+                                                .foregroundStyle(.background)
+                                        }
+                                        Text(tagItem.tagTitle)
+                                            .bold()
+                                            .padding(.horizontal)
+                                            .padding(.top, 4)
+                                            .foregroundColor(.white)
+                                    }
+                                    .frame(height: 70)
+                                    .frame(maxWidth: .infinity)
                                 }
-                                Text(tagItem.tagTitle)
-                                    .bold()
-                                    .padding(.horizontal)
-                                    .padding(.top, 4)
-                                    .foregroundColor(.white)
+                                .background(
+                                    Color.purple.gradient.opacity(0.8),
+                                    in: RoundedRectangle(
+                                        cornerRadius: 16,
+                                        style: .continuous
+                                    )
+                                )
                             }
-                            .frame(height: 70)
-                            .frame(maxWidth: .infinity)
                         }
-                        .background(
-                            Color.purple.gradient.opacity(0.8),
-                            in: RoundedRectangle(
-                                cornerRadius: 16,
-                                style: .continuous
-                            )
-                        )
+                        .padding()
                     }
-                }
-                .padding()
-            }
-            .task {
-                if metaData.tags.tagItems.isEmpty {
-                    await metaData.tags.createTags(posts)
+                    .task {
+                        if metaData.tags.tagItems.isEmpty {
+                            await metaData.tags.createTags(posts)
+                        }
+                    }
                 }
             }
             .navigationTitle("Suchen")
             .navigationBarTitleDisplayMode(.inline)
             .selectNavigationDestination()
+            .searchable(text: $searchText, prompt: "Inhalt durchsuchen")
+            .searchFocused($isSearchFieldFocused)
         }
     }
 }
