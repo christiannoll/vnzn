5e442674d2aed964dbbcfacd8a599005c7ba0f1f
Christian Noll <christiannoll@gmx.net>
2026-01-12 16:23:44 +0100

    Use only one button for sorting at PersonsView

--- vnzn/View/PersonsView.swift
+++ vnzn/View/PersonsView.swift
@@ -7,6 +7,8 @@ struct PersonsView: View {
     @Environment(Router.self) var router: Router
     @Environment(MetaData.self) var metaData: MetaData
 
+    @State private var sortOrderReversed: Bool = false
+
     var body: some View {
         List {
             ForEach(metaData.persons.register.registerItems, id: \.self) { item in
@@ -29,13 +31,8 @@ struct PersonsView: View {
         }
         .toolbar {
             ToolbarItem(placement: .topBarTrailing) {
-                Button("sortDown", systemImage: "chevron.down") {
-                    metaData.persons.register.registerItems.sort()
-                }
-            }
-            ToolbarItem(placement: .topBarTrailing) {
-                Button("sortUp", systemImage: "chevron.up") {
-                    metaData.persons.register.registerItems.sort(by: >)
+                Button("sort", systemImage: "chevron.up.chevron.down") {
+                    sort()
                 }
             }
         }
@@ -45,5 +42,14 @@ struct PersonsView: View {
             }
         }
         .navigationTitle("Personen")
+    }
+
+    private func sort() {
+        sortOrderReversed.toggle()
+        if sortOrderReversed {
+            metaData.persons.register.registerItems.sort(by: >)
+        } else {
+            metaData.persons.register.registerItems.sort()
+        }
     }
 }
