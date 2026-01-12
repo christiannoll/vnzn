4f6c03be086303768c7abbf5ea1f52a932f8b78e
Christian Noll <christiannoll@gmx.net>
2026-01-12 21:28:35 +0100

    Use rounded corner for images in PastView

--- vnzn/View/Experiments/PastView.swift
+++ vnzn/View/Experiments/PastView.swift
@@ -25,6 +25,7 @@ struct PastView: View {
                                 } label: {
                                     PostImage(post: post)
                                         .frame(width: 200, height: 200)
+                                        .cornerRadius(12)
                                 }
                                 Spacer()
                             }
