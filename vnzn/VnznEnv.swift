1f142852737871baf8d74c37f476a5f3c8d9202a
Christian Noll <christiannoll@gmx.net>
2026-02-01 13:01:13 +0100

    Unifying the deployment target

--- vnzn/VnznEnv.swift
+++ vnzn/VnznEnv.swift
@@ -5,14 +5,14 @@ struct VnznEnv {
     static var baseUrl: String {
         if let lang = Locale.currentLanguage {
             if lang.contains("en") {
-                return "http://localhost:8000/en/"
-                // return "https://www.vnzn.de/en/"
+                //return "http://localhost:8000/en/"
+                return "https://www.vnzn.de/en/"
             }
         }
-        return "http://localhost:8000/"
-        // return "https://www.vnzn.de/"
+        //return "http://localhost:8000/"
+        return "https://www.vnzn.de/"
     }
 
-    public static let baseRootUrl = "http://localhost:8000/"
-    //public static let baseRootUrl = "https://www.vnzn.de/"
+    //public static let baseRootUrl = "http://localhost:8000/"
+    public static let baseRootUrl = "https://www.vnzn.de/"
 }
