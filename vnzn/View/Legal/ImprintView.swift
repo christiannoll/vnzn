53aec7bc597b831a21d851d0555e0356e48d1e9c
Christian Noll <christiannoll@gmx.net>
2026-03-10 18:08:47 +0100

    Simplified ImprintView and TermsOfUseView

--- vnzn/View/Legal/ImprintView.swift
+++ vnzn/View/Legal/ImprintView.swift
@@ -3,17 +3,12 @@ import SwiftUI
 struct ImprintView: View {
     
     var body: some View {
-        VStack(alignment: .leading) {
-            HStack {
+        Form {
+            Section("Impressum") {
                 Text("v.n.z.n (Christian Noll)\nBautzner Straße 7b\n24837 Schleswig")
-                    .padding()
-                Spacer()
+                Text("Email: webmaster (at) vnzn (punkt) de\nGitHub: [christiannoll](https://github.com/christiannoll/vnzn)\nMastodon: [@vnzn@mas.to](https://mas.to/@vnzn)")
             }
-            Text("Email: webmaster (at) vnzn (punkt) de\nGitHub: [christiannoll](https://github.com/christiannoll/vnzn)\nMastodon: [@vnzn@mas.to](https://mas.to/@vnzn)")
-                .padding()
-            Spacer()
         }
-        .navigationTitle("Impressum")
     }
 }
 
--- vnzn/View/Legal/TermsOfUseView.swift
+++ vnzn/View/Legal/TermsOfUseView.swift
@@ -3,11 +3,10 @@ import SwiftUI
 struct TermsOfUseView: View {
     
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Die Nutzung von VnznApp unterliegt Apples Standard-EULA für Apps. In VnznApp bereitgestellte Trinkgelder schalten keine zusätzlichen Inhalte in der App frei.")
-                .padding()
-            Spacer()
+        Form {
+            Section("Nutzungsbedingungen") {
+                Text("Die Nutzung von VnznApp unterliegt Apples Standard-EULA für Apps. In VnznApp bereitgestellte Trinkgelder schalten keine zusätzlichen Inhalte in der App frei.")
+            }
         }
-        .navigationTitle("Nutzungsbedingungen")
     }
 }
