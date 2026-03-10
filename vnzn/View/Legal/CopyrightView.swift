882cd9e94e774d222039e3ed8e45e84588c8b225
Christian Noll <christiannoll@gmx.net>
2026-03-10 18:12:02 +0100

    Simplified CopyrightView

--- vnzn/View/Legal/CopyrightView.swift
+++ vnzn/View/Legal/CopyrightView.swift
@@ -3,14 +3,12 @@ import SwiftUI
 struct CopyrightView: View {
     
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("© Copyright 2019-2026 – Urheberrechtshinweis")
-                .padding()
-                .bold()
-            Text("Alle Inhalte dieses Internetangebotes, insbesondere Texte, Fotografien, Bilder und Grafiken sind urheberrechtlich geschützt. Das Urheberrecht liegt, soweit nicht ausdrücklich anders gekennzeichnet, bei Christian Noll. Bitte fragen Sie mich, falls Sie die Inhalte dieses Internetangebotes verwenden möchten.").padding(.horizontal)
-            Spacer()
+        Form {
+            Section("Copyright") {
+                Text("© Copyright 2019-2026 – Urheberrechtshinweis").bold()
+                Text("Alle Inhalte dieses Internetangebotes, insbesondere Texte, Fotografien, Bilder und Grafiken sind urheberrechtlich geschützt. Das Urheberrecht liegt, soweit nicht ausdrücklich anders gekennzeichnet, bei Christian Noll. Bitte fragen Sie mich, falls Sie die Inhalte dieses Internetangebotes verwenden möchten.")
+            }
         }
-        .navigationTitle("Copyright")
     }
 }
 
