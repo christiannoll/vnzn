5a05fba7f3d50cff4a964f6f88ed709002b65dbb
Christian Noll <christiannoll@gmx.net>
2026-07-12 12:20:31 +0200

    Removed unnecessary code in PostStatisticsData

--- vnzn/Statistics/PostStatisticData.swift
+++ vnzn/Statistics/PostStatisticData.swift
@@ -2,11 +2,11 @@ import Foundation
 
 struct PostStatisticData {
 
-    var publishDate: String = ""
-    var imagePost: Bool = false
+    var publishDate = ""
+    var imagePost = false
     var wordCount: Int = 0
     var linkCount: Int = 0
-    var serialPost: Bool = false
+    var serialPost = false
     var postItem: Post
     var visits: Int = 0
 }
